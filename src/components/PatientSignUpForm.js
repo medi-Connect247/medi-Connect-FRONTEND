c2cb2fc156340db60b7b454b94d43e414beea876
dannyy2000 <akinsanyadaniel665@gmail.com>
2023-06-01 15:07:31 -0700

    updated

--- src/components/PatientSignUpForm.js
+++ src/components/PatientSignUpForm.js
@@ -1,143 +1,184 @@
 import { useState } from "react";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../assets/med-logo_prev_ui.png";
 
-import "../pages/PatientSignUp.css"
-import axios from 'axios';
+import "../pages/PatientSignUp.css";
+import axios from "axios";
 
+function PatientSignUpForm() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordMatches, setPasswordMatches] = useState("");
+  const [error, setError] = useState("");
 
-function PatientSignUpForm(){
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [passwordMatches, setPasswordMatches] = useState('');
-    const [error, setError] = useState('');
-    
+  function handleFirstNameChange(event) {
+    const value = event.target.value;
+    if (/^[A-Za-z]*$/.test(value) && value.length <= 50) {
+      setFirstName(value);
+      setError("");
+    } else {
+      setError(
+        "First name should contain alphabets and can not exceed 50 characters."
+      );
+    }
+  }
 
-    function handleFirstNameChange(event) {
-        const value = event.target.value;
-        if (/^[A-Za-z]*$/.test(value) && value.length <= 50) {
-          setFirstName(value);
-          setError('');
-        } else {
-          setError('First name should contain alphabets and can not exceed 50 characters.');
-        }
-      };
-    
-      function handleLastNameChange(event) {
-        const value = event.target.value;
-        if (/^[A-Za-z]*$/.test(value) && value.length <= 50) {
-          setLastName(value);
-          setError('');
-        } else {
-          setError('Last name should contain alphabets and can not exceed 50 characters.');
-        }
-      };
+  function handleLastNameChange(event) {
+    const value = event.target.value;
+    if (/^[A-Za-z]*$/.test(value) && value.length <= 50) {
+      setLastName(value);
+      setError("");
+    } else {
+      setError(
+        "Last name should contain alphabets and can not exceed 50 characters."
+      );
+    }
+  }
 
-      function handleEmailChange(event) {
-        const value = event.target.value;
-        if (value.length <= 50) {
-          setEmail(value);
-          setError('');
-      
-          if (value.trim() === '') {
-            setError('Please enter your email address.');
-          } else if (!/\S+@\S+\.\S+/.test(value)) {
-            setError('Please enter a valid email address.');
-          }
-        } else {
-          setError('Email should not exceed 50 characters.');
-        }
-      };
-   
-      function handlePasswordChange(event) {
-        const value = event.target.value;
-        if (value.length <= 30) {
-          setPassword(value);
-          setError('');
-      
-          if (value.trim() === '') {
-            setError('Please enter your password.');
-          } else if (value.length < 6) {
-            setError('Password should be at least 6 characters long.');
-          }
-        } else {
-          setError('Password should not exceed 30 characters.');
-        }
+  function handleEmailChange(event) {
+    const value = event.target.value;
+    if (value.length <= 50) {
+      setEmail(value);
+      setError("");
+
+      if (value.trim() === "") {
+        setError("Please enter your email address.");
+      } else if (!/\S+@\S+\.\S+/.test(value)) {
+        setError("Please enter a valid email address.");
       }
-      
-      function handlePasswordConfirmationChange(event) {
-        const value = event.target.value;
-        setPasswordMatches(value);
-        setError('');
-      
-        if (value !== password) {
-          setError('Password and password confirmation do not match.');
-        }
-      };
+    } else {
+      setError("Email should not exceed 50 characters.");
+    }
+  }
 
-      function handleSubmit(event) {
-        event.preventDefault();
-      
-        if (
-          firstName.trim() === '' ||
-          lastName.trim() === '' ||
-          email.trim() === '' ||
-          password.trim() === '' ||
-          passwordMatches.trim() === ''
-        ) {
-          setError('Please fill in all the fields.');
-          return;
-        }
+  function handlePasswordChange(event) {
+    const value = event.target.value;
+    if (value.length <= 30) {
+      setPassword(value);
+      setError("");
 
-        axios.post('http://localhost:8080/api/v1/user/register',{firstName,lastName,email,password,passwordMatches})
-        .catch(error => console.error(error))
+      if (value.trim() === "") {
+        setError("Please enter your password.");
+      } else if (value.length < 6) {
+        setError("Password should be at least 6 characters long.");
+      }
+    } else {
+      setError("Password should not exceed 30 characters.");
+    }
+  }
+
+  function handlePasswordConfirmationChange(event) {
+    const value = event.target.value;
+    setPasswordMatches(value);
+    setError("");
+
+    if (value !== password) {
+      setError("Password and password confirmation do not match.");
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (
+      firstName.trim() === "" ||
+      lastName.trim() === "" ||
+      email.trim() === "" ||
+      password.trim() === "" ||
+      passwordMatches.trim() === ""
+    ) {
+      setError("Please fill in all the fields.");
+      return;
     }
 
+    axios
+      .post("http://localhost:8080/api/v1/user/register", {
+        firstName,
+        lastName,
+        email,
+        password,
+        passwordMatches,
+      })
+      .catch((error) => console.error(error));
+  }
 
-
-           
-    return(
-     <div>
-        <Link to="/"><button type="submit" className='b-button'>Back to Homepage</button></Link>
-        <img src={logo} alt="Logo" className="authentication-logo" />
-        <div className="sign-up-section">
-            <div className="p-sign-up-image"></div>
-            <div className="patient-sign-up-right">
-                <h2 className="sign-up-header">Create an Account</h2>
-                <p className="sign-up-sub-text">Let's get you started</p>
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        <input type="text" placeholder="First Name" value={firstName} onChange={handleFirstNameChange} className="sign-up-input"/>
-                    </label>
-                    <label>
-                        <input type="text" placeholder="Last Name" value={lastName} onChange={handleLastNameChange} className="sign-up-input"/>
-                    </label>
-                    <label>
-                        <input type="text" placeholder="Email" value={email} onChange={handleEmailChange} className="sign-up-input"/>
-                    </label>
-                    <label>
-                        <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange} className="sign-up-input"/>
-                    </label>
-                    <label>
-                        <input type="password" placeholder="Confirm Password" value={passwordMatches} onChange={handlePasswordConfirmationChange} className="sign-up-input"/>
-                    </label>
-                    {error && <p>{error}</p>}
-                    <button type="submit" className='acct-btn'>Create Account</button>
-                    <div className="patient-sn-container">
-                        <p className="patient-sn-last-text">Already have an account?</p>
-                        <Link to="/PatientLogin"><a href="" className="patient-sn-a-tag">Login</a></Link>
-                    </div>
-                    
-                    </form>
-                   
+  return (
+    <div>
+      <Link to="/">
+        <button type="submit" className="b-button">
+          Back to Homepage
+        </button>
+      </Link>
+      <img src={logo} alt="Logo" className="authentication-logo" />
+      <div className="sign-up-section">
+        <div className="p-sign-up-image"></div>
+        <div className="patient-sign-up-right">
+          <h2 className="sign-up-header">Create an Account</h2>
+          <p className="sign-up-sub-text">Let's get you started</p>
+          <form onSubmit={handleSubmit}>
+            <label>
+              <input
+                type="text"
+                placeholder="First Name"
+                value={firstName}
+                onChange={handleFirstNameChange}
+                className="sign-up-input"
+              />
+            </label>
+            <label>
+              <input
+                type="text"
+                placeholder="Last Name"
+                value={lastName}
+                onChange={handleLastNameChange}
+                className="sign-up-input"
+              />
+            </label>
+            <label>
+              <input
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={handleEmailChange}
+                className="sign-up-input"
+              />
+            </label>
+            <label>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={handlePasswordChange}
+                className="sign-up-input"
+              />
+            </label>
+            <label>
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                value={passwordMatches}
+                onChange={handlePasswordConfirmationChange}
+                className="sign-up-input"
+              />
+            </label>
+            {error && <p>{error}</p>}
+            <button type="submit" className="acct-btn">
+              Create Account
+            </button>
+            <div className="patient-sn-container">
+              <p className="patient-sn-last-text">Already have an account?</p>
+              <Link to="/PatientLogin">
+                <a href="" className="patient-sn-a-tag">
+                  Login
+                </a>
+              </Link>
             </div>
+          </form>
         </div>
-     </div>
-      );
-    
-
+      </div>
+    </div>
+  );
 }
 export default PatientSignUpForm;
-
--- src/components/PractitionerLoginForm.js
+++ src/components/PractitionerLoginForm.js
@@ -2,107 +2,131 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/med-logo_prev_ui.png";
 import "../pages/PractitionerLogin.css";
-import validator from 'email-validator';
-import axios from 'axios';
+import validator from "email-validator";
+import axios from "axios";
 
-function PractitionerLoginForm(){
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+function PractitionerLoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-      
-    function handleEmailChange(event){
-        const value = event.target.value.trim();
-        setEmail(value);
-        setError('');
-        if (value === '') setError('Email is required.');
-        else if (!isValidEmail(value) && !validator.validate(value)) setError('Please enter a valid email address.');
-      };
-      
-    function handlePasswordChange(event){
-        const value = event.target.value.trim();
-        setPassword(value);
-        setError('');
-        if (value === '') setError('Password is required.');
-        else if (!isValidPassword(value)) setError('Please enter a valid password.');
+  function handleEmailChange(event) {
+    const value = event.target.value.trim();
+    setEmail(value);
+    setError("");
+    if (value === "") setError("Email is required.");
+    else if (!isValidEmail(value) && !validator.validate(value))
+      setError("Please enter a valid email address.");
+  }
 
-      };
+  function handlePasswordChange(event) {
+    const value = event.target.value.trim();
+    setPassword(value);
+    setError("");
+    if (value === "") setError("Password is required.");
+    else if (!isValidPassword(value))
+      setError("Please enter a valid password.");
+  }
 
-      function isValidEmail(email) {
-        const emailRegex = /^\S+@\S+\.\S+.{50}$/;
-        return emailRegex.test(email);
-      };
+  function isValidEmail(email) {
+    const emailRegex = /^\S+@\S+\.\S+.{50}$/;
+    return emailRegex.test(email);
+  }
 
-      function isValidPassword(password) {
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,30}$/;
-        return passwordRegex.test(password);
-      };
+  function isValidPassword(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,30}$/;
+    return passwordRegex.test(password);
+  }
 
-      function handleSubmit(event) {
-        event.preventDefault();
+  function handleSubmit(event) {
+    event.preventDefault();
 
-        if (email.trim() === '' || password.trim() === '') {
-            setError('Please fill in all the fields.');
-            return;
-          }
-
-          if (!validator.validate(email)) {
-            setError('Please enter a valid email address.');
-            return;
-          }
-        
-          if (!isValidPassword(password)) {
-            setError('Please enter a valid password.');
-            return;
-          }
-
-
-        setEmail('');
-        setPassword('');
-
-        const login = async () => {
-          const res = await axios.post(
-            "http://localhost:8080/api/v1/user/authenticate",
-            { email, password }
-          );
-          if (res.status === 200) {
-            const { access_token, refresh_token } = res.data;
-            console.log("access", access_token);
-            sessionStorage.setItem("accessToken", access_token);
-            sessionStorage.setItem("refreshToken", refresh_token);
-          }
-        };
-        login();
+    if (email.trim() === "" || password.trim() === "") {
+      setError("Please fill in all the fields.");
+      return;
     }
 
-      return(
-        <div>
-             <Link to="/"><button type="submit" className='prac-back-button'>Back to Homepage</button></Link>
-             <img src={logo} alt="Logo" className="authentication-logo" />
-             <div className="login-section">
-                <div className="login-image"></div>
-                <div className="login-right">
-                    <h2 className="login-header">Welcome Back</h2>
-                    <p className="login-sub-text">Continue from where you stopped</p>
-                    <form onSubmit={handleSubmit}>
-                    <label>
-                        <input type="text" placeholder="Email" value={email} onChange={handleEmailChange} className="login-input"/>
-                    </label>
-                    <label>
-                        <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange} className="login-input"/>
-                    </label>
-                    {error && <p>{error}</p>}
-                    <Link to="/AccountSuccess"><button type="submit" className='login-btn'>Login</button></Link>
-                    <button type="submit" className='login-btn'>Login</button>
-                    <div className="container">
-                        <p className="last-text">Don't have an account?</p>
-                        <Link to="/PractitionerSignUp"><a  href="" className="a-tag">Create an account</a></Link>
-                    </div>
-                    
-                    </form>
-                </div>
-             </div>
+    if (!validator.validate(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!isValidPassword(password)) {
+      setError("Please enter a valid password.");
+      return;
+    }
+
+    setEmail("");
+    setPassword("");
+
+    const login = async () => {
+      const res = await axios.post(
+        "http://localhost:8080/api/v1/user/authenticate",
+        { email, password }
+      );
+      if (res.status === 200) {
+        const { access_token, refresh_token } = res.data;
+        console.log("access", access_token);
+        sessionStorage.setItem("accessToken", access_token);
+        sessionStorage.setItem("refreshToken", refresh_token);
+      }
+    };
+    login();
+  }
+
+  return (
+    <div>
+      <Link to="/">
+        <button type="submit" className="prac-back-button">
+          Back to Homepage
+        </button>
+      </Link>
+      <img src={logo} alt="Logo" className="authentication-logo" />
+      <div className="login-section">
+        <div className="login-image"></div>
+        <div className="login-right">
+          <h2 className="login-header">Welcome Back</h2>
+          <p className="login-sub-text">Continue from where you stopped</p>
+          <form onSubmit={handleSubmit}>
+            <label>
+              <input
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={handleEmailChange}
+                className="login-input"
+              />
+            </label>
+            <label>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={handlePasswordChange}
+                className="login-input"
+              />
+            </label>
+            {error && <p>{error}</p>}
+            <Link to="/AccountSuccess">
+              <button type="submit" className="login-btn">
+                Login
+              </button>
+            </Link>
+            <button type="submit" className="login-btn">
+              Login
+            </button>
+            <div className="container">
+              <p className="last-text">Don't have an account?</p>
+              <Link to="/PractitionerSignUp">
+                <a href="" className="a-tag">
+                  Create an account
+                </a>
+              </Link>
+            </div>
+          </form>
         </div>
-      )
+      </div>
+    </div>
+  );
 }
 export default PractitionerLoginForm;
